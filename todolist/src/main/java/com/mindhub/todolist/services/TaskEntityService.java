7d7501ed6281213ba9282907bafc62baa54cdca4
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-09 11:48:58 -0300

    Added GetAll method

--- todolist/src/main/java/com/mindhub/todolist/services/TaskEntityService.java
+++ todolist/src/main/java/com/mindhub/todolist/services/TaskEntityService.java
@@ -5,9 +5,12 @@ import com.mindhub.todolist.dtos.TaskEntityDTO;
 import com.mindhub.todolist.exceptions.TaskNotFoundException;
 import com.mindhub.todolist.models.TaskEntity;
 
+import java.util.List;
+
 public interface TaskEntityService {
     TaskEntityDTO getTaskDTOById(Long id) throws TaskNotFoundException;
     TaskEntity getTaskById(Long id) throws TaskNotFoundException;
+    List<TaskEntity> getAllTasks();
     TaskEntity updateTask(Long id, TaskEntity taskEntity) throws TaskNotFoundException; // PUT
     void saveTask(TaskEntity taskEntity);
     void deleteTask(Long id); // DELETE
--- todolist/src/main/java/com/mindhub/todolist/services/UserEntityService.java
+++ todolist/src/main/java/com/mindhub/todolist/services/UserEntityService.java
@@ -4,11 +4,15 @@ import com.mindhub.todolist.dtos.NewUserEntityDTO;
 import com.mindhub.todolist.dtos.UserEntityDTO;
 import com.mindhub.todolist.exceptions.UserNotFoundException;
 import com.mindhub.todolist.models.UserEntity;
+import org.apache.catalina.User;
+
+import java.util.List;
 
 public interface UserEntityService {
     UserEntityDTO getUserDTOById(Long id) throws UserNotFoundException;
     UserEntity getUserById(Long id) throws UserNotFoundException;
     UserEntity updateUser(Long id, UserEntity userEntity) throws UserNotFoundException;
+    List<UserEntity> getAllUsers();
     void saveUser(UserEntity userEntity);
     void deleteUser(long id); // DELETE
     void createNewUserEntity(NewUserEntityDTO newUserEntityDTO); //POST
